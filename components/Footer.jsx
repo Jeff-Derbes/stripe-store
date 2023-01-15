ea76b213f8f38c781fd19c44e3f6754d27f645c7
Jeff Derbes <derbes.jeffrey@gmail.com>
2023-01-15 12:47:31 -0600

    converted footer to tailwind

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,9 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 function Footer() {
   return (
-    <div className="footer-container">
+    <div className=" text-center mt-16 py-16 px-10 flex flex-col items-center gap-8 justify-center">
       <p>2022 JD Shop All rights reserverd</p>
-      <p className="icons">
+      <p className="flex text-2xl">
         <AiFillInstagram />
         <AiOutlineTwitter />
       </p>
