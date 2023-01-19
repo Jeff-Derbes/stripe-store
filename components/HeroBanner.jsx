3bbf37acbe02851ad30c5730a4e3531eea0f8671
Jeff Derbes <derbes.jeffrey@gmail.com>
2023-01-19 06:21:48 -0600

    updated hero banner to use tailwind

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,21 +5,35 @@ import { urlFor } from "../lib/client";
 
 function HeroBanner({ heroBanner }) {
   return (
-    <div className="hero-banner-container">
+    <div className=" py-[100px] px-[80px] bg-gray-200 md:h-[500px] relative rounded-2xl w-full mt-20">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
+        <p className="text-2xl">{heroBanner.smallText}</p>
+        <h3 className="text-6xl pt-4">{heroBanner.midText}</h3>
         <img
           src={urlFor(heroBanner.image)}
-          className="hero-banner-image"
+          className=" md:absolute top-[10%] right-[15%] w-[550px]"
           alt="hero-banner-image"
         />
-        <div>
+        <div className="pt-8">
           <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 mr-2 -ml-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+              </svg>
+              {heroBanner.buttonText}
+            </button>
           </Link>
 
-          <div className="desc">
+          <div className=" absolute right-[10%] bottom-[5%] w-[300px] flex flex-col text-end">
             <p>{heroBanner.desc}</p>
           </div>
         </div>
